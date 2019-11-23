077d83ac4114ffaae595eb200be3b3e318499b01
ema11412 <ema11412@hotmail.com>
2019-11-23 07:53:06 -0600

    Login "nice"

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict'
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars')
+const app = express();
+const api = require('./routes')
+const path = require('path');
+const parser = require('body-parser');
+
+app.use('/static', express.static('views'))
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.engine('.html', hbs({
+  defaultlayout: 'default',
+  extname: '.html' 
+}))
+
+
+app.set('view engine', '.html')
+
+app.use('/api', api)
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login22.html'));
+})
+
+app.get('/init', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+  //res.sendFile(path.join(__dirname, '/views/inicio.html'));
+})
+
+/*app.get('/login.html', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+  //res.sendFile(path.join(__dirname, '/views/inicio.html'));
+})
+*/
+//Verificar
+app.use(parser());
+app.use(express.static("id"));
+
+app.post("/login", function(req,res){
+    var id = req.body.id;
+    if(id == "666"){
+        res.redirect("/init");
+    }else{
+        res.redirect("/static/login.html")
+    }
+});
+
+module.exports = app
